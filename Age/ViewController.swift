fd42fe6f492d8a5c38e85d415fd122e1b30fa5b7
davidtsong <davidsongis@gmail.com>
2018-06-13 09:56:17 -0700

    Initial Commit

---
+++ Age/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Age
+//
+//  Created by David Song on 6/13/18.
+//  Copyright © 2018 David Song. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
