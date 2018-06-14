baa6f81b81c32b11937fd190ba3a0d9b84c0013e
davidtsong <davidsongis@gmail.com>
2018-06-13 23:41:01 -0700

    Initial commit v1.0 for the life tracker, it works with text labeling, time tracking, and launch on start working with saving

---
+++ Age/EventMonitor.swift
@@ -0,0 +1,9 @@
+//
+//  EventMonitor.swift
+//  Age
+//
+//  Created by David Song on 6/13/18.
+//  Copyright © 2018 David Song. All rights reserved.
+//
+
+import Foundation
---
+++ Age/MenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuViewController.swift
+//  Age
+//
+//  Created by David Song on 6/13/18.
+//  Copyright © 2018 David Song. All rights reserved.
+//
+
+import Cocoa
+
+class MenuViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
