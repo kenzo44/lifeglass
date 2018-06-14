76c9026e8bbc56074bced7e0fb82ed10d1cb3485
davidtsong <davidsongis@gmail.com>
2018-06-13 23:48:25 -0700

    refactor and also fixed initial birthdate set bug where it would fatal error because it thought birthdate was nil

--- Age/AppDelegate.swift
+++ Age/AppDelegate.swift
@@ -11,16 +11,118 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    let popover = NSPopover()
+    var eventMonitor: EventMonitor?
+    var timer:Timer?
+    var birth: Date!
+    var birthdayexists = false
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        if let button = statusItem.button {
+//            button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+            var birthdate = UserDefaults.standard.object(forKey: "Birthdate")
+            if let birthTemp = birthdate as? Date //Initializes the birth Date object
+            {
+                birth = birthTemp
+                birthdayexists = true
+                runTimer()
+                //Start if bday already exists
+            }
+            else
+            {
+                button.title = "Click Here and Set Birthdate" // No birthdate found, then replace with default blank
+            }
+            button.action = #selector(togglePopover(_:))
+        }
+        
+        popover.contentViewController = MenuViewController.freshController()
+        
+        //Start Countdown / CountUp
+        
+        
+        eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
+            if let strongSelf = self, strongSelf.popover.isShown {
+                strongSelf.closePopover(sender: event)
+            }
+        }
+    }
+    func updateTimer()
+    {
+        var birthdate = UserDefaults.standard.object(forKey: "Birthdate")
+        if let birthTemp = birthdate as? Date //Initializes the birth Date object
+        {
+            birth = birthTemp
+            birthdayexists = true
+            runTimer()
+            //Start if bday already exists
+        }
+    }
+    func timerRunning() -> Bool
+    {
+        return (timer !== nil)
+    }
+    func runTimer()
+    {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Counting), userInfo: nil, repeats: true)
+    }
+    func stopTimer()
+    {
+        timer?.invalidate()
+    }
+    @objc func Counting()
+    {
+        //Approach #2 to calculating the difference in time
+        var calendar = Calendar.current
+        var times = calendar.dateComponents([.year,.month,.day,.hour,.minute,.second], from:birth, to:Date())
+        
+        //Approach #1 to calculating the difference in time
+        //            let interval = Int(birth.timeIntervalSinceNow) * -1
+        //            let years = interval / 31536000
+        //            let months = (interval % 31536000) / 2628000
+        //            let days = (interval % 31536000 % 2628000) / 86400
+        //            let hours = (interval % 31536000 % 2628000 % 86400) / 3600
+        //            let minutes = (interval % 31536000 % 2628000 % 86400 % 3600) / 60
+        //            let seconds = (interval % 31536000 % 2628000 % 86400 % 3600 % 60)
+        //            statusItem.button?.title = "\(years)y:\(months)m:\(days)d:\(hours)h:\(minutes)m:\(seconds)s"
+        if(UserDefaults.standard.bool(forKey:"Labeling"))
+        {
+        statusItem.button?.title = "\(times.year!)y:\(times.month!)m:\(times.day!)d:\(times.hour!)h:\(times.minute!)m:\(times.second!)s"
+        }
+        else
+        {
+            statusItem.button?.title = "\(times.year!):\(times.month!):\(times.day!):\(times.hour!):\(times.minute!):\(times.second!)"
+        }
+        
+        
+
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+    
+    @objc func togglePopover(_ sender: Any?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
+    
+    func showPopover(sender: Any?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+        
+        eventMonitor?.start()
+    }
+    
+    func closePopover(sender: Any?) {
+        popover.performClose(sender)
+        eventMonitor?.stop()
+    }
 
 }
 
--- Age/MenuViewController.swift
+++ Age/MenuViewController.swift
@@ -7,12 +7,97 @@
 //
 
 import Cocoa
+import ServiceManagement
 
 class MenuViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
-    }
+    @IBOutlet var datePicker: NSDatePicker!
+    @IBOutlet var saveButton: NSButton!
+    @IBOutlet weak var birthdayText: NSTextField!
+    @IBOutlet weak var labelingButton: NSButton!
+    @IBOutlet weak var launchstartButton: NSButtonCell!
     
+    override func viewDidLoad()
+    {
+        let birthdate = UserDefaults.standard.object(forKey: "Birthdate")
+        if let birth = birthdate as? Date
+        {
+            datePicker.dateValue = birth
+            
+            birthdayText.stringValue = "Birthday is already set"
+//            saveButton.isEnabled = false
+        }
+        
+    }
 }
+
+extension MenuViewController{
+    static func freshController() -> MenuViewController {
+        
+        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+        
+        let identifier = NSStoryboard.SceneIdentifier(rawValue: "MenuViewController")
+        
+        guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? MenuViewController else {
+            fatalError("Why cant i find MenuViewController? - Check Main.storyboard")
+        }
+        
+        
+        return viewcontroller
+    }
+
+}
+extension MenuViewController {
+    @IBAction func quit(_sender: NSButton) {
+         NSApplication.shared.terminate(_sender)
+    }
+    @IBAction func textLabelingToggle(_sender:  NSButton)
+    {
+        let appDelegate = NSApp.delegate as! AppDelegate
+        switch labelingButton.state {
+        case .on:
+            UserDefaults.standard.set(true, forKey:"Labeling")
+        case .off:
+            UserDefaults.standard.set(false, forKey:"Labeling")
+        default: break
+        }
+        appDelegate.stopTimer()
+        //Start Timer Need to reset birthdate value
+        appDelegate.updateTimer()
+    }
+    @IBAction func launchToggle(_sender: NSButton)
+    {
+        let launcherAppId = "com.tiborbodecs.LauncherApplication"
+        switch labelingButton.state {
+        case .on:
+              SMLoginItemSetEnabled(launcherAppId as CFString, true)
+        case .off:
+              SMLoginItemSetEnabled(launcherAppId as CFString, false)
+        default: break
+        }
+    }
+    @IBAction func save(_sender: NSButton) {
+        let appDelegate = NSApp.delegate as! AppDelegate
+        let birthdate = datePicker.dateValue
+        
+        birthdayText.stringValue = "Birthday is already set"
+        UserDefaults.standard.set(birthdate, forKey:"Birthdate")
+//        saveButton.isEnabled = false
+//        if(!appDelegate.timerRunning())
+//        {
+//            appDelegate.runTimer()
+//        }
+//        else
+//        {
+            //Stop Timer
+            appDelegate.stopTimer()
+            //Start Timer Need to reset birthdate value 
+            appDelegate.updateTimer()
+//        }
+    }
+
+}
+
+
+
+
+
